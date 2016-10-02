0a4543a95d95c90d46ed394064cdab6240b16f0b
apanangadan <apanangadan@fullerton.edu>
2016-10-02 01:59:39 -0700

    Added ArrayStack.h and main

---
+++ ArrayStack.h
@@ -0,0 +1,66 @@
+// Code based on:
+// Data Structures and Algorithms in C++, Goodrich, Tamassia, and Mount, 2nd Ed., 2011.
+//
+#pragma once
+
+#include <stdexcept>
+
+using namespace std;
+
+template <typename E>
+class ArrayStack {
+	enum { DEF_CAPACITY = 100 };		// default stack capacity
+public:
+	ArrayStack(int cap = DEF_CAPACITY);		// constructor from capacity
+	int size() const;				// number of items in the stack
+	bool empty() const;				// is the stack empty?
+	const E& top() const;	// get the top element
+	void push(const E& e);	// push element onto stack
+	void pop();				// pop the stack
+	void printAll();					// print all elements on stack to cout
+private:                                	// member data
+	E* S;					// array of stack elements
+	int capacity;				// stack capacity
+	int t;					// index of the top of the stack
+};
+
+template <typename E> ArrayStack<E>::ArrayStack(int cap)
+	: S(new E[cap]), capacity(cap), t(-1) { }	// constructor from capacity
+
+template <typename E> int ArrayStack<E>::size() const
+{
+	return (t + 1);
+}				// number of items in the stack
+
+template <typename E> bool ArrayStack<E>::empty() const
+{
+	return (t < 0);
+}				// is the stack empty?
+
+template <typename E>				// return top of stack
+const E& ArrayStack<E>::top() const {
+	if (empty()) throw length_error("Top of empty stack");
+	return S[t];
+}
+
+template <typename E>				// push element onto the stack
+void ArrayStack<E>::push(const E& e) {
+	if (size() == capacity) throw length_error("Push to full stack");
+	S[++t] = e;
+}
+
+template <typename E>				// pop the stack
+void ArrayStack<E>::pop() {
+	if (empty()) throw length_error("Pop from empty stack");
+	--t;
+}
+
+// print all elements on stack
+template <typename E>
+void ArrayStack<E>::printAll() {
+	if (empty()) throw length_error("Empty stack");
+	cout << "Elements in stack: ";
+	for (int i = t; i >= 0; i--)
+		cout << "{" << S[i] << "} ";
+	cout << endl;
+}
---
+++ ArrayStack_main.cpp
@@ -0,0 +1,44 @@
+//
+//  main for ArrayStack
+//
+
+#include <iostream>
+#include "ArrayStack.h"
+
+int main(int argc, const char * argv[]) {
+	// create a stack
+	ArrayStack<int> intStack;
+	try {
+		intStack.push(5);
+		intStack.push(3);
+		int size = intStack.size();
+		cout << "Size of the stack now: " << size << endl;
+		intStack.printAll();
+		intStack.pop();
+		intStack.printAll();
+		intStack.push(7);
+		intStack.printAll();
+		intStack.pop();
+		intStack.printAll();
+		cout << "Top of the stack = " << intStack.top() << endl;
+		intStack.printAll();
+		intStack.pop();
+		intStack.printAll();
+		cout << "Top of the stack = " << intStack.top() << endl;
+		intStack.printAll();
+	}
+	catch (length_error &err) {
+		cout << "Error condition: " << err.what() << endl;
+	}
+	try {
+		intStack.top();
+		intStack.printAll();
+		bool isEmpty = intStack.empty();
+		cout << "Is stack empty (yes=true, no=false)" << isEmpty << endl;
+	}
+	catch (length_error &err) {
+		cout << "Error condition: " << err.what() << endl;
+	}
+
+	return EXIT_SUCCESS;
+}
